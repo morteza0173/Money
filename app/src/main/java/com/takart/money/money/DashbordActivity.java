5ef3d753a65b6aedd32fce1d1bb9d7966458406d
morteza0173 <morteza0173@outlook.com>
2018-09-15 17:27:08 +0430

    some change

--- app/src/main/java/com/takart/money/money/DashbordActivity.java
+++ app/src/main/java/com/takart/money/money/DashbordActivity.java
@@ -1,7 +1,6 @@
 package com.takart.money.money;
 
 import android.animation.ValueAnimator;
-import android.renderscript.Sampler;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,6 +20,7 @@ public class DashbordActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashbord);
         final int[] addMoneyMath = {0};
+        final int[] spendMoneyMath = {0};
 
 
         final boolean[] addmoney1 = {true};
@@ -41,7 +41,15 @@ public class DashbordActivity extends AppCompatActivity {
         final TextView txtViewAddMoney = (TextView)findViewById(R.id.textView3_addMoney);
         final EditText edttextloseMoney = (EditText)findViewById(R.id.editTextLoseMoney);
         final TextView txtViewloseMoney = (TextView)findViewById(R.id.textViewLoseMoney);
-        final TextView txtViewAllMoneyYouHave = (TextView)findViewById(R.id.textView_allMoneyYouHave);
+        final TextView txtViewAllMoneyYouEarn = (TextView)findViewById(R.id.textView_allMoneyYouEarn);
+        final TextView moneyYouEarnForWeak = (TextView)findViewById(R.id.moneyYouEarnForWeak);
+        final TextView moneyYouEarnForMonth = (TextView)findViewById(R.id.moneyYouEarnForMonth);
+        final TextView allMoneyYouSpend = (TextView)findViewById(R.id.allMoneyYouSpend);
+        final TextView allMoneyYouSpendForWeak = (TextView)findViewById(R.id.allMoneyYouSpendForWeak);
+        final TextView allMoneyYouSpendForMonth = (TextView)findViewById(R.id.allMoneyYouSpendForMonth);
+        TextView allMoneyYouHave = (TextView)findViewById(R.id.allMoneyYouHave);
+        final TextView moneyYouHaveInMoneyYouSpend = (TextView)findViewById(R.id.moneyYouHaveInMoneyYouSpend);
+        final TextView moneyYouSpendMoreThanYouEarn = (TextView)findViewById(R.id.moneyYouSpendMoreThanYouEarn);
 
         addMoneyButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -75,16 +83,83 @@ public class DashbordActivity extends AppCompatActivity {
                     addMoneyButton.setText("افزودن درآمد");
                     edttextAddMoney.setVisibility(View.GONE);
                     txtViewAddMoney.setVisibility(View.GONE);
-                    int moneyYouHave = Integer.valueOf(txtViewAllMoneyYouHave.getText().toString());
+                    int moneyYouHave = Integer.valueOf(txtViewAllMoneyYouEarn.getText().toString());
                     addMoneyMath[0] += Integer.valueOf(edttextAddMoney.getText().toString());
                     ValueAnimator allMoneyYouHaveAnimation = ValueAnimator.ofInt(moneyYouHave , addMoneyMath[0]);
                     allMoneyYouHaveAnimation.setDuration(200);
                     allMoneyYouHaveAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
                         @Override
                         public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                            txtViewAllMoneyYouHave.setText(valueAnimator.getAnimatedValue().toString());
+                            txtViewAllMoneyYouEarn.setText(valueAnimator.getAnimatedValue().toString());
+                            moneyYouEarnForWeak.setText(valueAnimator.getAnimatedValue().toString());
+                            moneyYouEarnForMonth.setText(valueAnimator.getAnimatedValue().toString());
+
                         }
                     });allMoneyYouHaveAnimation.start();
+
+                    float allMoneyYouSpend1 = Float.valueOf(allMoneyYouSpend.getText().toString());
+                    float txtViewAllMoneyYouEarn1 = addMoneyMath[0];
+
+                    float moneyYouSpendInYouEarn = allMoneyYouSpend1 / txtViewAllMoneyYouEarn1 ;
+                    final float moneyYouSpendInYouEernPercent = moneyYouSpendInYouEarn * 100;
+                    final int moneyYouSpendInYouEernPercentInt = (int) moneyYouSpendInYouEernPercent;
+                    int moneyYouSpendInYouEernPercentBefore = Integer.valueOf(moneyYouHaveInMoneyYouSpend.getText().toString());
+                    ValueAnimator moneyYouSpendInYouEernPercentAnimation = ValueAnimator.ofInt(moneyYouSpendInYouEernPercentBefore, moneyYouSpendInYouEernPercentInt);
+                    moneyYouSpendInYouEernPercentAnimation.setDuration(200);
+                    moneyYouSpendInYouEernPercentAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                            if (moneyYouSpendInYouEernPercentInt < 100) {
+                                moneyYouHaveInMoneyYouSpend.setText(valueAnimator.getAnimatedValue().toString());
+                            }else {
+                                moneyYouHaveInMoneyYouSpend.setText("100");
+                            }
+
+                        }
+                    });moneyYouSpendInYouEernPercentAnimation.start();
+
+
+                    int allMoneyYouSpend2 = Integer.valueOf(allMoneyYouSpend.getText().toString());
+                    int txtViewAllMoneyYouEarn2 = addMoneyMath[0];
+                    final int moneyYouSpellMoreThanYouEarn = allMoneyYouSpend2 - txtViewAllMoneyYouEarn2;
+
+                    int moneyYouSpellMoreThanYouEarnBefore = Integer.valueOf(moneyYouSpendMoreThanYouEarn.getText().toString());
+
+
+                        if (moneyYouSpellMoreThanYouEarn>0){
+                            ValueAnimator moneyYouSpellMoreThanYouEarnAnimation = ValueAnimator.ofInt(moneyYouSpellMoreThanYouEarnBefore , moneyYouSpellMoreThanYouEarn);
+                            moneyYouSpellMoreThanYouEarnAnimation.setDuration(200);
+                            moneyYouSpellMoreThanYouEarnAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                                @Override
+                                public void onAnimationUpdate(ValueAnimator valueAnimator) {
+
+
+                                    moneyYouSpendMoreThanYouEarn.setText(valueAnimator.getAnimatedValue().toString());
+
+                                }
+                            });moneyYouSpellMoreThanYouEarnAnimation.start();
+                        }
+
+                        if(moneyYouSpellMoreThanYouEarn<= 0){
+                        ValueAnimator moneyYouSpellMoreThanYouEarnAnimation = ValueAnimator.ofInt(moneyYouSpellMoreThanYouEarnBefore , 0);
+                        moneyYouSpellMoreThanYouEarnAnimation.setDuration(200);
+                        moneyYouSpellMoreThanYouEarnAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                            @Override
+                            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+
+
+                                moneyYouSpendMoreThanYouEarn.setText(valueAnimator.getAnimatedValue().toString());
+
+                            }
+                        });moneyYouSpellMoreThanYouEarnAnimation.start();
+
+                    }
+
+
+
+
+
+
 
 
 
@@ -123,6 +198,45 @@ public class DashbordActivity extends AppCompatActivity {
                     loseMoney.setText("افزودن خرج");
                     edttextloseMoney.setVisibility(View.GONE);
                     txtViewloseMoney.setVisibility(View.GONE);
+                    int moneyYouSpend = Integer.valueOf(allMoneyYouSpend.getText().toString());
+                    spendMoneyMath[0] += Integer.valueOf(edttextloseMoney.getText().toString());
+                    ValueAnimator allMoneyYouSpendAnimation = ValueAnimator.ofInt(moneyYouSpend , spendMoneyMath[0]);
+                    allMoneyYouSpendAnimation.setDuration(200);
+                    allMoneyYouSpendAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                            allMoneyYouSpend.setText(valueAnimator.getAnimatedValue().toString());
+                            allMoneyYouSpendForWeak.setText(valueAnimator.getAnimatedValue().toString());
+                            allMoneyYouSpendForMonth.setText(valueAnimator.getAnimatedValue().toString());
+
+                        }
+                    });allMoneyYouSpendAnimation.start();
+
+
+                      int allMoneyYouSpend1 = spendMoneyMath[0];
+                      int txtViewAllMoneyYouEarn1 = Integer.valueOf(txtViewAllMoneyYouEarn.getText().toString());
+
+
+                      if (allMoneyYouSpend1 > txtViewAllMoneyYouEarn1) {
+                    final int moneyYouSpellMoreThanYouEarn = allMoneyYouSpend1 - txtViewAllMoneyYouEarn1;
+                    int moneyYouSpellMoreThanYouEarnBefore = Integer.valueOf(moneyYouSpendMoreThanYouEarn.getText().toString());
+                    if (moneyYouSpellMoreThanYouEarn>0){
+                        ValueAnimator moneyYouSpellMoreThanYouEarnAnimation = ValueAnimator.ofInt(moneyYouSpellMoreThanYouEarnBefore , moneyYouSpellMoreThanYouEarn);
+                        moneyYouSpellMoreThanYouEarnAnimation.setDuration(200);
+                        moneyYouSpellMoreThanYouEarnAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                            @Override
+                            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+
+
+                                moneyYouSpendMoreThanYouEarn.setText(valueAnimator.getAnimatedValue().toString());
+
+                            }
+                        });moneyYouSpellMoreThanYouEarnAnimation.start();
+                    }
+
+                   }
+
+
                 }
 
             }
